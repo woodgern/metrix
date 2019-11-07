b98d99dfc45ca21576371b0c311d54e9a7d21a89
Kaiser Dandangi <kdandangi@freshbooks.com>
2019-11-06 19:42:07 -0500

    add a post endpoint for metrics

--- metrix/src/main.rs
+++ metrix/src/main.rs
@@ -1,13 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 // #[macro_use] extern crate diesel;
-#[macro_use] extern crate diesel_migrations;
+// #[macro_use] extern crate diesel_migrations;
 
-embed_migrations!();
+// embed_migrations!();
 
 pub mod lib;
 
-use lib::establish_connection;
+// use lib::establish_connection;
 // use metrix::schema::metrics::dsl::*;
 
 #[get("/ping")]
@@ -15,13 +15,18 @@ fn ping() -> &'static str {
     "pong"
 }
 
+#[post("/metrics")]
+fn create_metrics() -> &'static str {
+    "metrics"
+}
+
 fn main() {
     println!("### Enter the Metrix ###");
-    let db_conn = establish_connection();
+    // let db_conn = establish_connection();
 
     println!("### running db migrations...");
-    let result = embedded_migrations::run(&db_conn);
-    println!("### migration done; result: {}", result.is_ok());
+    // let result = embedded_migrations::run(&db_conn);
+    // println!("### migration done; result: {}", result.is_ok());
 
-    rocket::ignite().mount("/metrix", routes![ping]).launch();
+    rocket::ignite().mount("/metrix", routes![ping, create_metrics]).launch();
 }
