995bc8a4bd090e554029014ace1c913472e47829
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2019-11-29 02:14:46 -0500

    we got tests baby!

--- metrix/src/main.rs
+++ metrix/src/main.rs
@@ -16,6 +16,10 @@ pub mod app;
 
 use lib::establish_connection;
 
+pub fn create_app() -> rocket::Rocket {
+  app::create_app()
+}
+
 fn main() {
     println!("### Enter the Metrix ###");
     let db_conn = establish_connection();
@@ -24,5 +28,22 @@ fn main() {
     let result = embedded_migrations::run(&db_conn);
     println!("### migration done; result: {}", result.is_ok());
 
-    app::create_app().launch();
+    create_app().launch();
+}
+
+#[cfg(test)]
+mod test {
+    use super::create_app;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn ping_me_baby() {
+      let client = Client::new(create_app()).expect("valid rocket instance");
+
+      let mut response = client.get("/ping").dispatch();
+
+      assert_eq!(response.status(), Status::Ok);
+      assert_eq!(response.body_string(), Some("pong".into()));
+    }
 }
