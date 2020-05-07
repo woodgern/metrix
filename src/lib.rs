796ff8fc121e519123c57fcedc6ee62c1aa61f64
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2020-05-06 23:15:26 -0400

    setup DB pooling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate serde;
 
@@ -10,6 +11,11 @@ pub mod parser;
 pub mod schema;
 
 mod routes;
+
+// This registers your database with Rocket, returning a `Fairing` that can be `.attach`'d to your
+// Rocket application to set up a connection pool for it and automatically manage it for you.
+#[database("postgres_db")]
+pub struct DbConn(diesel::PgConnection);
 
 pub fn create_app() -> rocket::Rocket {
     rocket::ignite()
@@ -23,4 +29,5 @@ pub fn create_app() -> rocket::Rocket {
             routes::metrics::query_metric_params,
             routes::metrics::search_metric_names
         ])
+        .attach(DbConn::fairing())
 }
